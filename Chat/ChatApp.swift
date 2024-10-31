15f3b1dde82a308b2ffcfec872189e010cc26f1d
Ilia Proskuriakov <prosk300@gmail.com>
2024-10-31 17:17:24 +0600

    Initial commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Ilia Proskuriakov on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ChatView()
+        }
+    }
+}
